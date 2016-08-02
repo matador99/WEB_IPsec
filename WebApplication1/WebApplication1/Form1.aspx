7d7b2b4937ca7ad9fd2ea7808df60b5f58d0cbc8
Sasha <panteleev405@gmail.com>
2016-08-02 22:57:14 +0300

    WEB_IPsec

---
+++ WebApplication1/WebApplication1/Form1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="WebApplication1.Form1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 450px;
+            width: 350px;
+            background-color: gainsboro;
+        }
+        #UKR, #USA, #RUS, #EUR{
+            Height:40px;
+            Width:125px;
+            position: relative;
+            top: 250px;
+            left: 50px;
+            background-color: lightslategrey;
+        }
+    </style>
+</head>
+<body style="height: 450px; width: 350px">
+    <form id="form1" runat="server">
+        <asp:TextBox ID="Login" runat="server"></asp:TextBox>
+        <p>
+        <asp:Button ID="UKR" runat="server" OnClick="Button1_Click" Text="Ukraine" />
+        <asp:Button ID="RUS" runat="server" OnClick="Button1_Click" Text="Russia" />
+        <asp:Button ID="USA" runat="server" OnClick="Button1_Click" Text="USA" />
+        <asp:Button ID="EUR" runat="server" OnClick="Button1_Click" Text="EUR" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Form1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Form1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(@"Data Source=SashaPC\SQLExpress;Initial Catalog=IPsec;User ID=IPUser;Password=matador99");
+
+            try
+            {
+                con.Open();
+            }
+            catch
+            {
+             }
+            
+            string sqlEnter = "EXECUTE [dbo].[GetUser] @LoginName='Sasha' ,@Password='matador99'";
+            
+            System.Data.SqlClient.SqlCommand cmdEnter = new System.Data.SqlClient.SqlCommand(sqlEnter, con);
+            
+            System.Data.SqlClient.SqlDataReader drEnter = cmdEnter.ExecuteReader();
+            
+            if (drEnter.Read())
+            {
+            Login.Text = drEnter[1].ToString();
+            }
+            
+            else
+            {
+            Login.Text = "False";
+            }
+        }
+
+        
+    }
+}
